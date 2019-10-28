a50df45ae1f7865f3323b1bc294ba21fda5cef26
VictorHCord <victorhugocordeiro1997@gmail.com>
2019-10-27 22:42:48 -0300

    GoBarber

---
+++ src/routes.js
@@ -0,0 +1,43 @@
+// Estou pegando somente "Router" de dentro de express
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleControlle';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
+
+import authMiddleware from './app/middlewares/auth';
+// Estou instanciando Router e colocando dentro de routes
+const routes = new Router();
+
+const upload = multer(multerConfig);
+
+// Estou criando uma rota pra criação de um usuario
+// Está importando a rota e fazendo a criação via post
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,5 @@
+// Aqui é a onde eu irei criar o servidor
+// Ele ira ser a onde a rota está hospedada
+import app from './app';
+
+app.listen(3333);
